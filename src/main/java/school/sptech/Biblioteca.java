4df8181b50dbffd9f2c6d82c7afd6faf60d82b89
MatheusFerroTorres <matheus.torres@sptech.school>
2024-10-23 19:52:32 -0300

    Primeiro commit do exercicio

--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,61 @@
 package school.sptech;
 
-public class Biblioteca {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Biblioteca {
+    private String nome;
+    private List<Livro> livros;
+
+    public Biblioteca() {
+    }
+
+    public Biblioteca(String nome) {
+        this.nome = nome;
+        this.livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        if (livro !=null && (livro.getTitulo()!=null && livro.getTitulo().isBlank())&&(livro.getAutor()!=null && livro.getAutor().isBlank())&&(livro.getDataPublicacao()!=null)) {
+            Livro novoLivro = new Livro();
+            livros.add(novoLivro);
+            System.out.println("Livro criado!");
+        }
+    }
+
+    public void removerLivroPorTitulo(String titulo){
+    List<Livro> listaLivro = new ArrayList<>();
+        for (Livro livroDaVez : livros){
+            if (livroDaVez.getTitulo().equalsIgnoreCase(titulo)){
+                listaLivro.remove(livroDaVez);
+            }
+        }
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo) {
+        for (Livro livroDaVez : livros) {
+            if (livroDaVez.getTitulo().equalsIgnoreCase(titulo) && (livroDaVez.getTitulo() != null && !livroDaVez.getTitulo().isBlank())) {
+                return livroDaVez;
+            }
+        }
+        return null;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
 }
--- src/main/java/school/sptech/Livro.java
+++ src/main/java/school/sptech/Livro.java
@@ -1,5 +1,79 @@
 package school.sptech;
 
-public class Livro {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Livro {
+    private String titulo;
+    private String autor;
+    private LocalDate dataPublicacao;
+    private List<Avaliacao> avaliacoes;
+
+    public Livro() {
+    }
+
+    public Livro(String titulo, String autor, LocalDate dataPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.dataPublicacao = dataPublicacao;
+        this.avaliacoes = new ArrayList<>();
+    }
+
+    public void adicionarAvaliacao(String descricao, Double qtdEstrelas){
+        if (descricao!=null&&!descricao.isBlank()&&(qtdEstrelas >=0 && qtdEstrelas <=5)) {
+            Avaliacao novaAvaliacao = new Avaliacao();
+            avaliacoes.add(novaAvaliacao);
+            System.out.println("Avaliação criada!");
+        }
+    }
+
+    public Double calcularMediaAvaliacoes(){
+        Double total = 0.0;
+        Double media = 0.0;
+        if (avaliacoes.isEmpty()){
+                return 0.0;
+            } else{
+                for (int i = 0; i < avaliacoes.size(); i++){
+                    total += avaliacoes.get(i).getQtdEstrelas();
+                media = total / avaliacoes.size();
+                }
+            }
+        return media;
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", dataPublicacao=" + dataPublicacao +
+                ", avaliacoes=" + avaliacoes +
+                '}';
+    }
 }
